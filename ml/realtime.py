b597e73bc1d7ac059aaae6d6ed5cbde77854d104
ivanvb <ivanvalette@hotmail.com>
2021-04-04 04:21:50 -0400

    add dev commands to python

--- ml/realtime.py
+++ ml/realtime.py
@@ -11,12 +11,12 @@ sio.connect(url)
 
 @sio.event
 def connect():
-    print("I'm connected!")
+    print("I'm connected!!!")
 
 @sio.on("prueba")
 def on_message(data):
     global x
-    print('I received a message!')
+    print('I received a message!!!!')
 
     x = x+1
     return f"OK prueba {x}"
